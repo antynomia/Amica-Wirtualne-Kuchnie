23eb51232e8d56e66ea6362a3771ddedadc6daf2
antynomia <lidia.wasko@gmail.com>
2024-08-16 14:39:17 +0200

    topmenu

--- scripts.js
+++ scripts.js
@@ -1,8 +1,8 @@
 // Initialize animations
 const animations = [
-    { id: 'sprzety', path: 'ico-anim/sprzet.json' },
-    { id: 'sprzety', path: 'ico-anim/sprzet.json' },
-    { id: 'sprzety', path: 'ico-anim/sprzet.json' },
+    { id: 'sprzety', path: 'https://antynomia.github.io/Amica-Wirtualne-Kuchnie/ico-anim/sprzet.json' },
+    { id: '3d', path: 'https://antynomia.github.io/Amica-Wirtualne-Kuchnie/ico-anim/3d.json' },
+    { id: 'pomysly', path: 'https://antynomia.github.io/Amica-Wirtualne-Kuchnie/ico-anim/pomysly.json' },
 
 ];
 
